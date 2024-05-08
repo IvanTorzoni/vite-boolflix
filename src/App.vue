31833b5e5f53b3ef8cca443c43c76108920c31cf
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-05-08 16:22:20 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import test from "./components/test.vue";
+
+export default {
+  components: {
+    test
+  },
+}
+</script>
+
+<template>
+  <test />
+</template>
+
+<style lang="scss"></style>
